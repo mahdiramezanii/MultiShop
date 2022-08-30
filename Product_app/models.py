996edce2a781ed46fc0eee911a356e50e18ddd6a
mahdiramazani <mahdiramazani1281@gmail.com>
2022-08-30 17:39:08 +0430

    Add the product model to the project] and upload the product model and categories to the admin panel

--- Product_app/models.py
+++ Product_app/models.py
@@ -4,7 +4,7 @@ from Acount_app.models import User
 
 class Category(models.Model):
     name=models.CharField(max_length=50)
-    image=models.ImageField(upload_to="product/image")
+    image=models.ImageField(upload_to="product/category")
     parent=models.ForeignKey("self",related_name="comment",on_delete=models.CASCADE)
     created=models.DateTimeField(auto_now_add=True)
 
@@ -13,5 +13,17 @@ class Category(models.Model):
 
         return self.name
 
+class Product(models.Model):
+    name=models.CharField(max_length=200)
+    image=models.ImageField(upload_to="product/image")
+    price=models.CharField(max_length=50)
+    discount=models.CharField(max_length=50,null=True,blank=True)
+    score=models.IntegerField(null=True)
+    category=models.ManyToManyField(Category,related_name="product")
+    created=models.DateTimeField(auto_now_add=True)
 
+
+    def __str__(self):
+
+        return self.name
 
