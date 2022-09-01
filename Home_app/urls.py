8407afa33dc47e208372c4c3a1c1a5f2d0d50882
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-01 10:53:48 +0430

    Add the ability to remove the product from the shopping cart to the project

--- Home_app/urls.py
+++ Home_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name="Home_app"
 urlpatterns=[
     path("",views.Home.as_view(),name="Home"),
-    path("product/<slug:slug>",views.DetailProduct.as_view(),name="detail"),]
+    path("product/<slug:slug>",views.DetailProduct.as_view(),name="detail"),
+    path("cart",views.ShopCart.as_view(),name="cart"),
+    path("cart/delete/<int:pk>",views.DeleteShopCart.as_view(),name="delet")
+]
 
--- Home_app/views.py
+++ Home_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, ListView, View, DetailView
+from django.views.generic import TemplateView, ListView, View, DetailView,DeleteView
 from Product_app.models import Product, Category, Comment, Cart, CartDetail
-from django.urls import reverse
+from django.urls import reverse,reverse_lazy
 
 
 class Home(View):
@@ -22,28 +22,62 @@ class DetailProduct(DetailView):
     template_name = "Home_app/detail.html"
 
     def post(self, request, slug):
-        product = Product.objects.get(slug=slug)
-        user = request.user
-        body = request.POST.get("body")
-        if user and body is not None:
-            Comment.objects.create(user=user, body=body, product=product, name=user.get_full_name(), email=user.email)
-            return redirect(reverse("Home_app:detail", kwargs={"slug": slug}))
 
-        else:
+        if request.user.is_authenticated:
 
-            color = request.POST.get("color")
-            size = request.POST.get("size")
-            count = request.POST.get("count")
+            product = Product.objects.get(slug=slug)
             user = request.user
-
-            if Cart.objects.filter(user=user).exists():
-                cart = Cart.objects.filter(user=user).last()
-                CartDetail.objects.create(cart=cart, product=product, price=product.price, color=color, size=size,
-                                          count=count)
+            body = request.POST.get("body")
+            if user and body is not None:
+                Comment.objects.create(user=user, body=body, product=product, name=user.get_full_name(), email=user.email)
                 return redirect(reverse("Home_app:detail", kwargs={"slug": slug}))
 
             else:
-                cart = Cart.objects.create(user=user)
-                CartDetail.objects.create(cart=cart,product=product, price=product.price, color=color, size=size,
+
+                color = request.POST.get("color")
+                size = request.POST.get("size")
+                count = request.POST.get("count")
+                user = request.user
+
+                if color and size and count:
+
+                    if Cart.objects.filter(user=user).exists():
+                        cart = Cart.objects.filter(user=user).last()
+                        CartDetail.objects.create(cart=cart, product=product, price=product.price, color=color, size=size,
                                           count=count)
+                        return redirect(reverse("Home_app:detail", kwargs={"slug": slug}))
+
+                    else:
+                        cart = Cart.objects.create(user=user)
+                        CartDetail.objects.create(cart=cart,product=product, price=product.price, color=color, size=size,
+                                            count=count)
+                        return redirect(reverse("Home_app:detail", kwargs={"slug": slug}))
+
                 return redirect(reverse("Home_app:detail", kwargs={"slug": slug}))
+
+        return redirect(reverse("Home_app:detail", kwargs={"slug": slug}))
+
+
+class ShopCart(ListView):
+    model=DetailProduct
+    template_name = "Home_app/cart.html"
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+
+            if Cart.objects.filter(user=self.request.user).exists():
+                cart=Cart.objects.filter(user=self.request.user).last()
+                query_set=CartDetail.objects.filter(cart=cart)
+                if query_set.exists():
+                    return query_set
+                return None
+
+        else:
+            query_set=None
+            return query_set
+
+
+class DeleteShopCart(DeleteView):
+    model = CartDetail
+    success_url = reverse_lazy("Home_app:cart")
+
