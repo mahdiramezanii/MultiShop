20e933c67fd01600b07a0b4a58675bce0dafaebe
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-01 09:27:26 +0430

    Adding the ability to add products to the shopping cart

--- Home_app/urls.py
+++ Home_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name="Home_app"
 urlpatterns=[
     path("",views.Home.as_view(),name="Home"),
-    path("product/<slug:slug>",views.DetailProduct.as_view(),name="detail")
-]
+    path("product/<slug:slug>",views.DetailProduct.as_view(),name="detail"),]
+
--- Home_app/views.py
+++ Home_app/views.py
@@ -1,33 +1,49 @@
-from django.shortcuts import render,redirect
-from django.views.generic import TemplateView,ListView,View,DetailView
-from Product_app.models import Product,Category,Comment
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, View, DetailView
+from Product_app.models import Product, Category, Comment, Cart, CartDetail
 from django.urls import reverse
+
 
 class Home(View):
 
-    def get(self,request):
-        product=Product.objects.all()
-        category=Category.objects.all()
-        context={
-            "category":category,
-            "product":product,
+    def get(self, request):
+        product = Product.objects.all()
+        category = Category.objects.all()
+        context = {
+            "category": category,
+            "product": product,
         }
 
+        return render(request, "Home_app/index.html", context=context)
 
-        return render(request,"Home_app/index.html",context=context)
 
 class DetailProduct(DetailView):
     model = Product
     template_name = "Home_app/detail.html"
 
-    def post(self,request,slug):
-        product=Product.objects.get(slug=slug)
-        user=request.user
-        body=request.POST.get("body")
-        Comment.objects.create(user=user,body=body,product=product,name=user.get_full_name(),email=user.email)
+    def post(self, request, slug):
+        product = Product.objects.get(slug=slug)
+        user = request.user
+        body = request.POST.get("body")
+        if user and body is not None:
+            Comment.objects.create(user=user, body=body, product=product, name=user.get_full_name(), email=user.email)
+            return redirect(reverse("Home_app:detail", kwargs={"slug": slug}))
 
-        return redirect(reverse("Home_app:detail",kwargs={"slug":slug}))
+        else:
 
+            color = request.POST.get("color")
+            size = request.POST.get("size")
+            count = request.POST.get("count")
+            user = request.user
 
+            if Cart.objects.filter(user=user).exists():
+                cart = Cart.objects.all().last()
+                CartDetail.objects.create(cart=cart, product=product, price=product.price, color=color, size=size,
+                                          count=count)
+                return redirect("Home_app:Home")
 
-
+            else:
+                cart = Cart.objects.create(user=user)
+                CartDetail.objects.create(cart=cart, product=product, price=product.price, color=color, size=size,
+                                          count=count)
+                return redirect("Home_app:Home")
